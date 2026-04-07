909dcd45143fc6dceb0b6fb0c99235a09b57159f
habd1 <Shmily@Ha1>
2026-04-07 16:25:50 +0700

    Week 4 - Tuesday: Override

--- src/main/java/week4_inheritance/LoginPage.java
+++ src/main/java/week4_inheritance/LoginPage.java
@@ -3,11 +3,29 @@ package week4_inheritance;
 // LoginPage "LÀ MỘT" Page (Inheritance)
 public class LoginPage extends BasePage {
 
+    // Week 4 - Monday: Implement Inheritance with BasePage
+//    public void login(String user, String pass) {
+//        waitForPageLoaded(); // Hàm của cha
+//        sendKeys("Username Field", user); // Hàm của cha
+//        sendKeys("Password Field", pass); // Hàm của cha
+//        clickElement("Login Button"); // Hàm của cha
+//        System.out.println("Đăng nhập thành công!");
+//    }
+
+    // Week 4 - Tuesday: Override
+    // Ghi đè lại hàm clickElement của BasePage
+    @Override
+    public void clickElement(String elementName) {
+        System.out.println("[Security Check] Đang kiểm tra mã độc trước khi click...");
+        // Gọi lại hàm gốc của Cha nếu vẫn muốn dùng logic cũ
+        super.clickElement(elementName);
+        System.out.println("[Log] Click thành công nút bảo mật.");
+    }
+
     public void login(String user, String pass) {
-        waitForPageLoaded(); // Hàm của cha
-        sendKeys("Username Field", user); // Hàm của cha
-        sendKeys("Password Field", pass); // Hàm của cha
-        clickElement("Login Button"); // Hàm của cha
-        System.out.println("Đăng nhập thành công!");
+        waitForPageLoaded();
+        sendKeys("Username Field", user);
+        sendKeys("Password Field", pass);
+        clickElement("Login Button"); // Lúc này nó sẽ gọi hàm đã được Overwrite ở trên
     }
 }
--- src/main/java/week4_inheritance/RegisterPage.java
+++ src/main/java/week4_inheritance/RegisterPage.java
@@ -2,8 +2,15 @@ package week4_inheritance;
 
 public class RegisterPage extends BasePage {
     public void register(String email, String phone) {
+        waitForPageLoaded();
         sendKeys("Email Field", email); // Hàm của cha
         sendKeys("Phone Field", phone); // Hàm của cha
         clickElement("Register Button"); // Hàm của cha
+    }
+
+    @Override
+    public void waitForPageLoaded() {
+        System.out.println("Đang đợi trang Register tải dữ liệu từ Server VinFast...");
+        super.waitForPageLoaded();
     }
 }
